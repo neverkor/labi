2ca6ec0859fa63418761679b75454af53d18cc0c
neverkor <neverkor@gmail.com>
2018-02-09 17:25:39 +0300

    добавил в лабе 8 время выполнения поиска

--- laba8.py
+++ laba8.py
@@ -1,21 +1,34 @@
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 centr_x = float(input('Введите координату Х для центра окружности: '))
 centr_y = float(input('Введите координату Y для центра окружности: '))
 radius = float(input('Введите радиус окружности: '))
 
 # Количество точек
-N = 5000
+point = 150000
 
 # Генерация случайных координат для точек
-x = np.random.random_integers(-49, 49, N)
-y = np.random.random_integers(-49, 49, N)
+x = np.random.random_integers(-49, 49, point)
+y = np.random.random_integers(-49, 49, point)
 result_x = []
 result_y = []
 os_x = []
 os_y = y
-print(x, y)
+
+# Поиск точек по одной размерности
+point_x = float(input('Введите координату X: '))
+start = time.time()
+for key in x:
+    if key == point_x:
+        os_x.append(key)
+print('На этой координате найдено точек:', len(os_x))
+len_x = len(os_x)
+if len(os_x) != len(y):
+    os_y = os_y[:len_x]
+end = time.time()
+print('Время выполнения поиска точек:', end - start)
 
 # Циклы расчета по формуле (x-centr_x)**2 + (y - centr_y)**2 <= radius**2
 for key in x:
@@ -34,26 +47,13 @@ for key in result:
         not_enter += 1
 print('Входит или лежит на окружности точек:', enter, '\nНе входит:', not_enter)
 print('Центр окружности:', centr_x, ';', centr_y)
-
-# Поиск точек по одной размерности
-point_x = float(input('Введите координату X: '))
-for key in x:
-    if key == point_x:
-        os_x.append(key)
-print('На этой координате найдено точек:', len(os_x))
-len_x = len(os_x)
-if len(os_x) != len(y):
-    os_y = os_y[:len_x]
+print('Точек всего:', point)
 
 # Графическое изображение (не обязательно, так нагляднее)
 circle = plt.Circle((centr_x, centr_y), radius, color='blue', fill=False)
 fig, ax = plt.subplots()
 ax.add_artist(circle)
 ax.axis("equal")
-<<<<<<< HEAD
-=======
-print('Центр окружности:', centr_x, ';', centr_y)
->>>>>>> 7c63770bd6f6c367b389924a10a94180f710b669
 plt.xlim(-50, 50)
 plt.ylim(-50, 50)
 plt.scatter(x, y, s=0.5, color='red')
