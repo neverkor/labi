9f0be3fdfc622f4f2429836394e04b7b9750e0a2
neverkor <36150859+neverkor@users.noreply.github.com>
2018-02-14 14:48:04 +0300

    добавил обработку исключения для первой функции

--- laba2.py
+++ laba2.py
@@ -6,19 +6,19 @@ if choose == 1:
     try:
         g = 10 * (-45 * a ** 2 + 49 * a * x + 6 * x ** 2) / 15 * a ** 2 + 49 * a * x + 24 * x ** 2
         print('G =', g)
-    except:
+    except(ZeroDivisionError):
         print('Знаменатель обратился в 0.')
 elif choose == 2:
     try:
         f = tan(5 * a ** 2 + 34 * a * x + 45 * x ** 2)
         print('F =', f)
-    except:
+    except(ValueError):
         print('Введенные данные выходят за область значения функции F.')
 elif choose == 3:
     try:
         y = -asin(7 * a ** 2 - a * x - 8 * x ** 2)
         print('Y =', y)
-    except:
+    except(ValueError):
         print('Введенные данные выходят за область значения функции Y.')
 else:
     print('Нет такой функции.')
